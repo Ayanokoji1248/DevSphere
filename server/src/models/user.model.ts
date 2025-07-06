115014981e2c2807b5347976d587fff51b57dfa0
Krish <krishprajapati1248@gmail.com>
2025-07-07 02:24:54 +0530

    basic user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,1 +1,34 @@
-``
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+    username: String,
+    fullName: String,
+    avatar: String,
+    bannerImage: String,
+    devName: String,
+    bio: String,
+    skills: [String],
+    address: String,
+    porfolioLink: String,
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post"
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    }],
+    follower: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    }],
+    projects: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "project"
+    }]
+
+}, {
+    timestamps: true,
+})
+
+
