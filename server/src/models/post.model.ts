f5dd1c134162b8c4b142c6f3c492a20c39c23eb4
Krish <krishprajapati1248@gmail.com>
2025-07-08 12:25:25 +0530

    post schema added

---
+++ server/src/models/post.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    content: {
+        type: String,
+        minlength: [10, "Content must be at least 10 characters"],
+        maxlength: [5000, "Content cannot exceed 5000 characters"],
+        required: true,
+    },
+    code: {
+        type: String,
+        min: [5, "Atleast 5 Character"],
+        trim: true,
+    },
+    image: {
+        type: String,
+        trim: true,
+    },
+    link: {
+        type: String,
+        trim: true
+    },
+    tags: [String],
+
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comment"
+    }],
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    }]
+}, { timestamps: true });
+
+const postModel = mongoose.model("post", postSchema)
+
+export default postModel
