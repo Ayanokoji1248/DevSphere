98436aa0076ff47ba80679485e756e983d59c9db
Krish <krishprajapati1248@gmail.com>
2025-07-08 12:30:10 +0530

    comment model added

---
+++ server/src/models/comment.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    },
+    comment: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [1, "Cannot be empty"]
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+        required: true,
+    }
+}, {
+    timestamps: true,
+})
+
+const commentModel = mongoose.model("comment", commentSchema);
+
+export default commentModel
