75b0ec59413795b0afc523fe0b1a5bf95491c23c
Krish <krishprajapati1248@gmail.com>
2025-07-13 00:25:06 +0530

    `Refactor MainLayout component: update styles and layout for sticky elements and responsive design`

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -12,9 +12,9 @@ const MainLayout = () => {
         <div className="bg-black">
             <NavBar />
             <div className="md:w-[90%] min-h-screen  mx-auto flex gap-5 relative pt-18">
-                <div className="md:w-[25%] h-screen sticky p-3 pt-8 px-4 hidden md:block">
+                <div className="md:w-[25%] min-h-screen p-3 pt-8 px-4 hidden md:block">
 
-                    <div className="border-[1px] border-zinc-500  p-5 flex flex-col gap-4 rounded-xl text-white">
+                    <div className="sticky top-20 border-[1px] border-zinc-500  p-5 flex flex-col gap-4 rounded-xl text-white">
                         <h1 className="text-3xl font-medium">Quick Actions</h1>
                         <div className="flex flex-col gap-2">
                             <button className="p-2 text-lg font-medium font-[Albert_Sans] tracking-tighter bg-[#00FF9C] hover:bg-[#60f0bbea] transition-all duration-300 hover:-translate-y-0.5 text-white rounded-md cursor-pointer"> AI Code Review</button>
@@ -26,9 +26,9 @@ const MainLayout = () => {
                 <div className="md:w-[45%] w-full pt-4">
                     <Outlet />
                 </div>
-                <div className="md:w-[30%] hidden md:block h-screen border-2 p-7">
+                <div className="md:w-[30%] hidden md:block min-h-screen border-2 p-7">
                     {user &&
-                        <div className="border-[1px] border-zinc-500 p-5 flex flex-col gap-3 rounded-lg text-white">
+                        <div className="sticky top-20 border-[1px] border-zinc-500 p-5 flex flex-col gap-3 rounded-lg text-white">
                             <div className="flex gap-3 items-center">
                                 <div className="w-10 h-10 bg-zinc-700 rounded-full flex shrink-0"></div>
                                 <div className="font-[Albert_Sans] flex flex-col">
